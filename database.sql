b5982e75d31770e5aeac77793ded6d65dae91fc6
caahyadayu-create <caahyadayu@gmail.com>
2025-12-26 03:10:28 +0800

    Menambahkan file database SQL

--- database.sql
+++ database.sql
@@ -1,4 +1,9 @@
-CREATE TABLE tasks (
+-- Membuat Database
+CREATE DATABASE IF NOT EXISTS db_todo;
+USE db_todo;
+
+-- Membuat Tabel Tasks
+CREATE TABLE IF NOT EXISTS tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     task_name VARCHAR(255) NOT NULL
 );
